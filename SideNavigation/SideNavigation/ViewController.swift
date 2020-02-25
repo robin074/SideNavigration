985a03c3f552da7e9cfec3633ef50b6e6fdf007c
Apple MacBook Pro <Apple@Apples-MacBook-Pro-2.local>
2020-02-25 14:39:25 +0600

    sideNavigration

---
+++ SideNavigation/SideNavigation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SideNavigation
+//
+//  Created by Apple MacBook Pro on 2/3/20.
+//  Copyright © 2020 Apple MacBook Pro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
